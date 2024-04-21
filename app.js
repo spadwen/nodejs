05175af5307e9ef1d1abe834593523945799e427
spadwen <spa.dwen@gmail.com>
2024-04-21 18:14:42 +1000

    added form fields send to Gmail and saved to database

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
+const formRoutes = require('./formRoutes'); // Import formRoutes
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -27,5 +32,15 @@ app.get('/', (req, res) => res.render('index', { title: 'Home', carouselItems })
 app.get('/about', (req, res) => res.render('about', { title: 'About' }));
 app.get('/service', (req, res) => res.render('service', { title: 'Service' }));
 app.get('/contact', (req, res) => res.render('contact', { title: 'Contact' }));
+app.get('/thank-you', (req, res) => res.render('thank-you'));
+
+// Use the form submission route
+app.use('/', formRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
